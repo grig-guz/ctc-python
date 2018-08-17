3f5d8f833cd0ef870109a668f5de28381305546c
Grigorii Guz <prb578@icloud.com>
2018-08-16 20:05:05 -0700

    decoding test passed

--- ctc.py
+++ ctc.py
@@ -55,11 +55,83 @@ class CTCDecoder():
         return np.exp(alpha_matrix[T - 1, L - 1]) + np.exp(alpha_matrix[T - 1, L - 2])
 
     def best_path_decoding(self, output_timeseries):
+        """
+            The most elementary decoding scheme - take character predictions with
+            highest probabilities at each timestep. Ignores that there can be
+            multiple paths with higher summed probability corresponding to a
+            different label.
+        """
         best_path_indices = np.argmax(output_timeseries, axis = 1)
         best_path = [self.alphabet_reverse[i] for i in best_path_indices]
         return alignment_postprocess(best_path)
 
-#    def beam_search_decoding():
+    def beam_search_decoding(self, output_timeseries, beam_size):
+        """
+            Asymptotically correct decoding algorithm, but only asymptotically. Should probably
+            be ran on a GPU
+            output_timeseries       - T x D numpy array, where T
+                                      is the length of timeseries of character distributions,
+                                      and D is the size of the alphabet with the blank character
+            beam_size               - number of outputs to keep track of when expanding output_timeseries
+        """
+        # Initialization
+        first_timestep = output_timeseries[0]
+        if (beam_size > len(self.alphabet)):
+            # All paths can fit in the set of tracked beams
+            curr_beams = [(self.alphabet_reverse[i], (first_timestep[i], 0)) for i in range(len(self.alphabet))]
+        else:
+            best_alignments_ind = np.argpartition(first_timestep, -beam_size)[-beam_size:]
+            # Expand over beam_size most probable first characters, keeping the probability of
+            # this character
+            curr_beams = [(self.alphabet_reverse[i], (first_timestep[i], 0)) for i in best_alignments_ind]
+        #print('initialize')
+        #print(curr_beams)
+        #print('steps:')
+        # Step
+        for i, char_dist in enumerate(output_timeseries[1:], 1):
+            outputs_dict = {}
+            # Create dictionary where keys are new outputs and values are
+            # their probabilities
+            for beam in curr_beams:
+                output = beam[0]
+                # Probabilities of expanding one of the beams over next timestepbeam_size
+                expansion_probs = char_dist * (beam[1][0] + beam[1][1])
+
+                for new_char, j in self.alphabet.items():
+                    next_path = output + new_char
+                    # Ok, now it can be a repeat or blank or distinct, 3 cases
+                    # If blank,
+                    if new_char == '':
+                        if outputs_dict.get(output) is None:
+                            outputs_dict[output] = (0, expansion_probs[j])
+                        else:
+                            outputs_dict[output] = (outputs_dict[output][0], outputs_dict[output][1] + expansion_probs[j])
+                    # If repeat, we should keep two versions:
+                    # 1. the character added to the path is collapsed
+                    # 2. the character added to the path is not collapsed (so we have repeated characters in output, like 'll' in hello)
+                    elif len(output) > 0 and new_char == output[-1]:
+                        if outputs_dict.get(output) is None:
+                            outputs_dict[output] = (char_dist[j] * beam[1][0], 0)
+                        else:
+                            outputs_dict[output] = (char_dist[j] * beam[1][0] + outputs_dict[output][0], outputs_dict[output][1])
+                        if outputs_dict.get(next_path) is None:
+                            outputs_dict[next_path] = (char_dist[j] * beam[1][1], 0)
+                        else:
+                            outputs_dict[next_path] = (char_dist[j] * beam[1][1] + outputs_dict[next_path][0], outputs_dict[next_path][1])
+                    # If distinct, we just add this character
+                    else:
+                        if outputs_dict.get(next_path) is None:
+                            outputs_dict[next_path] = (expansion_probs[j], 0)
+                        else:
+                            outputs_dict[next_path] = (expansion_probs[j] + outputs_dict[next_path][0], outputs_dict[next_path][1])
+            #print(outputs_dict)
+            curr_beams = sorted(outputs_dict.items(), key = lambda t: t[1][0] + t[1][1], reverse = True)[:beam_size]
+            #print "\n"
+            #print(curr_beams)
+            #print "\n\n\n"
+
+        print((curr_beams[0][0], curr_beams[0][1][0] + curr_beams[0][1][1]))
+        return (curr_beams[0][0], curr_beams[0][1][0] + curr_beams[0][1][1])
 
     def preprocess_label(self, label):
         """ Converts the labels to a sequence of character codes with
@@ -98,7 +170,8 @@ def prod_of_logs(a, b):
 
 def alignment_postprocess(alignment):
     """
-        Removes repeated adjacent characters
+        Removes repeated adjacent characters. No need to remove blanks since
+        they are represented by '' here.
     """
     label = []
     label.append(alignment[0])
@@ -109,7 +182,6 @@ def alignment_postprocess(alignment):
 
 
 def test_eval_forward():
-    # Note: run tests without rescale_alpha
     alphabet1 = {'c': 0, 'a' : 1, 't' : 2, 'd' : 3, 'o':  4, 'g': 5, '': 6}
     alphabet2 = {'h': 0, 'e' : 1, 'l' : 2, 'o' : 3, '' : 4}
 
@@ -127,3 +199,21 @@ def test_eval_forward():
     print np.isclose(dec1.eval_forward_prob(output_timeseries_1, "cat"), 0.0007, 1e-9)
     print np.isclose(dec1.eval_forward_prob(output_timeseries_1, "dog"), 0.0007, 1e-9)
     print np.isclose(dec2.eval_forward_prob(output_timeseries_2, "hello"), 0.0001344, 1e-9)
+
+
+def test(i):
+    alphabet1 = {'c': 0, 'a' : 1, 't' : 2, 'd' : 3, 'o':  4, 'g': 5, '': 6}
+    alphabet2 = {'h': 0, 'e' : 1, 'l' : 2, 'o' : 3, '' : 4}
+
+    # Not valid distributions, but easy to compute
+    output_timeseries_1 = np.array([[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+                                    [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]])
+    # A bit more realistic
+    output_timeseries_2 = np.array([[0.3, 0.2, 0.1, 0.3, 0.1], [0.1, 0.5, 0.1, 0.2, 0.1], [0.2, 0.2, 0.2, 0.2, 0.2],
+                                    [0.6, 0.1, 0.1, 0.1, 0.1], [0.2, 0.2, 0.1, 0.3, 0.2], [0.1, 0.1, 0.1, 0.3, 0.1],
+                                    [0.1, 0.1, 0.1, 0.3, 0.1]])
+    dec1 = CTCDecoder(alphabet1)
+    dec2 = CTCDecoder(alphabet2)
+    #dec1.beam_search_decoding(output_timeseries_1, 3)
+    res_tuple = dec2.beam_search_decoding(output_timeseries_2, i)
+    print np.isclose(dec2.eval_forward_prob(output_timeseries_2, res_tuple[0]), res_tuple[1], 1e-9)
